2168784df416e0d8671d796e4fd3ce62f5390448
kathsydoruk <kathsydoruk@gmail.com>
2025-12-09 22:44:07 +0200

    L19 - config update

--- cypress.config.js
+++ cypress.config.js
@@ -2,14 +2,20 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://example.cypress.io/",
-  },
-  viewportWidth: 1920,
-  viewportHeight: 1080,
-  screenshotOnRunFailure: true,
-  video: false,
-  retries: {
-    runMode: 2,
-    openMode: 0,
+    baseUrl: "https://qauto.forstudy.space/",
+
+    env: {
+      authUsername: "guest",
+      authPassword: "welcome2qauto",
+    },
+
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    screenshotOnRunFailure: true,
+    video: false,
+    retries: {
+      runMode: 2,
+      openMode: 0,
+    },
   },
 });
